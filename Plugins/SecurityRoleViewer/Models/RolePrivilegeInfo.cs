300ba67e25ebf6e5540da2e641f1a6961ecac5cf
ComfyUI Attic <bobsdevattic@gmail.com>
2026-06-09 19:21:21 -0400

    feat: add RolePrivilegeInfo data model with access level mapping

---
+++ Plugins/SecurityRoleViewer/Models/RolePrivilegeInfo.cs
@@ -0,0 +1,28 @@
+namespace SecurityRoleViewer.Models
+{
+    public class RolePrivilegeInfo
+    {
+        public string RoleName { get; set; }
+        public string EntityName { get; set; }
+        public string PrivilegeName { get; set; }
+        public int Depth { get; set; }
+        public int ObjectTypeCode { get; set; }
+        public string Category { get; set; }
+
+        public string AccessLevelLabel
+        {
+            get
+            {
+                switch (Depth)
+                {
+                    case 0: return "None";
+                    case 1: return "User";
+                    case 2: return "Business Unit";
+                    case 4: return "Parent-Child BU";
+                    case 8: return "Organization";
+                    default: return "Unknown";
+                }
+            }
+        }
+    }
+}
